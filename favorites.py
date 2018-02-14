acf98d9e3dbf6681a18def772dda22bf14dd4003
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-02-14 08:38:06 -0500

    Updated from Brython Server: 2/14/2018 8:38:05 AM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,9 @@
+#Ryan Jones
+#2/14/18
+#favorites.py
 
+favword = input('Enter your favorite word: ')
+favnum = int(input('Enter your favorite number: '))
+
+for i in range(0,(favnum)):
+    print((favword))
