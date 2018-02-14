6f275104ed54d8d5f9d8ece8a004a286d06a2e46
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-02-14 08:17:07 -0500

    Updated from Brython Server: 2/14/2018 8:17:06 AM

--- loopDemo.py
+++ loopDemo.py
@@ -8,6 +8,12 @@ for i in range(1,6):
     print('I <3 Computer Science')
 """
 
+"""
 #Counts from 1 to 10
 for i in range(1,11):
+    print(i)
+"""
+
+#Count from 27 to 43 by 2s
+for i in range(27,44):
     print(i)
