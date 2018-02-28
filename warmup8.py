b4d8cd74d4bb2c852c8cddc997ed74dff84704ca
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-02-28 08:39:07 -0500

    Updated from Brython Server: 2/28/2018 8:39:05 AM

--- warmup8.py
+++ warmup8.py
@@ -2,6 +2,7 @@
 #2/28/18
 
 total = 0
-for i in range (1,100000,(3 and 10 and 17)):
-    total = total+i
-    print(total)
+for i in range (1,100000):
+    if i%3==0 and i%10==0 and i%17==0:
+        total = total+i
+print(total)
